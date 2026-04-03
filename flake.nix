41ecf9aec25c35ae49f3930e10fda1afae74b0fa
ollie <git@nuv.direct>
2026-04-03 13:44:15 +0200

    add flake.nix and .envrc

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "dev env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gleam
+            erlang_28
+            rebar3
+            bun
+          ];
+
+          shellHook = ''
+            echo "dev env loaded"
+            just --list
+            echo "Use just to run them."
+          '';
+        };
+      }
+    );
+}
